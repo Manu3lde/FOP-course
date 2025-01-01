98f3f925a9f59e65e9af88e47618f36bfb2fe89c
Manuel <blackonyxx20.4@gmail.com>
2025-01-01 19:14:57 +0300

    Binary to Decimal function beginning

---
+++ Binary-2-Decimal/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int bNum, D = 0, pwr;
+    cout << "Hello, This is a simple program to convert Binary numbers into Decimal" << endl;
+    cout << "Please Enter the binary number: ";
+    cin >> bNum;
+
+    while (bNum > 0) {
+        int digit = bNum % 10;
+        if (digit != 0 && digit != 1) {
+            cout << "Invalid binary number entered!" << endl;
+            return 1;
+        }
+        D += digit * pow(2, pwr);
+        bNum /= 10;             
+        pwr++;                   
+    }
+
+    cout << "The decimal equivalent is: " << D << endl;
+    return 0;
+}
